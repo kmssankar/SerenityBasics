1763467f5b68f28743d22d21bbca560aefc99c88
sankar <sankar@sankar-ThinkCentre-M90s-Gen-3>
2023-04-13 23:40:45 +0530

    Validate field availability

--- serenitybasics/src/test/java/org/sgs/serenity/BookStoreTests/UsersTest.java
+++ serenitybasics/src/test/java/org/sgs/serenity/BookStoreTests/UsersTest.java
@@ -3,13 +3,23 @@ package org.sgs.serenity.BookStoreTests;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
 import net.serenitybdd.core.steps.UIInteractions;
 
 import static net.serenitybdd.rest.SerenityRest.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Type;
+import java.util.List;
 
 import org.hamcrest.Matchers;
+import org.sgs.serenity.common.pojos.Root;
+
 
 public class UsersTest  extends UIInteractions{
+	
+	Response response;
 	
 	@Given("As a Authorized user with Valid access ")
 	public void createRequest(String token) {
@@ -20,7 +30,7 @@ public class UsersTest  extends UIInteractions{
 	
 	@When("when requests for Account info Account/v1/User/")
     public void whenRequestedForUserInformation(String uuid) {
-        when().get("/" + uuid);
+        response = when().get("/" + uuid);
     }
 	
 	
@@ -28,5 +38,46 @@ public class UsersTest  extends UIInteractions{
     public void thenISeeUserUuidAsResult(String uuid) {
         then().body("userId", Matchers.equalTo(uuid));
     }
+	
+	
+	public void validateAttributeValueInArray(String attribute , int value) {	
+		List<Integer> jsonArrayResponseValue = response.jsonPath().getList(attribute);
+		System.out.print(jsonArrayResponseValue);
+	    assertTrue(jsonArrayResponseValue.stream().allMatch(n-> n== value));
+		
+	}
 
+	//""
+	public <T> void validateAttributeAvailableInListResponse(String attributeList) {
+		Root root = response.as(Root.class);
+        String[] attributes = attributeList.split(",");
+		for( String eachAttribute : attributes) {
+			root.books.forEach(n-> assertTrue( n.getFieldValue(eachAttribute)!= null));	
+		}
+		
+		
+	}
+	
+	public static Object getFieldValue(Object object, String attribute) {
+		Object value =null;
+	try {
+			Field field = object.getClass().getDeclaredField(attribute);
+			value = field.get(object);
+			
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
 }
+
+
+
+
+
